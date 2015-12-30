06466e4290eecf717b300b487d31fe633ea8c6ae
Jon Austen <austenjt@gmail.com>
2015-12-29 21:00:25 -0800

    Got unit test working.

--- src/main/java/example/spring/common/controller/IndexController.java
+++ src/main/java/example/spring/common/controller/IndexController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
-public class HelloWorldController
+public class IndexController
 {
-    @RequestMapping(path={"/hello"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(path={"/index", "/"}, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-	protected HashMap<String, String> displayHelloWorldMessage()
+	protected HashMap<String, String> displayIndexMessage()
     {
         HashMap<String, String> model = new HashMap<>();
-        String name = "Hello World!!";
+        String name = "Index Page";
         model.put("greeting", name);
         return model;
 	}
