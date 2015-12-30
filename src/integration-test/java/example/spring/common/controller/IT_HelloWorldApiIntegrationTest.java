f3be0f03cdbff1847cb36fe690a98f7548032f99
Jon Austen <austenjt@gmail.com>
2015-12-29 21:33:09 -0800

    Almost got integration test working.

---
+++ src/integration-test/java/example/spring/common/controller/IT_HelloWorldApiIntegrationTest.java
@@ -0,0 +1,30 @@
+package example.spring.common.controller;
+
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.config.RestAssuredConfig;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Test;
+import static com.jayway.restassured.RestAssured.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.*;
+import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
+
+public class IT_HelloWorldApiIntegrationTest
+{
+    @Test
+    public void integrationTestIt()
+    {
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.setContentType("application/json; charset=UTF-8");
+        RequestSpecification requestSpec = builder.build();
+
+        Response response = given(requestSpec).when().get("http://localhost:8080/hello");
+        JSONObject jsonResponseObject = new JSONObject(response.body().asString());
+        String result = jsonResponseObject.get("greeting").toString();
+        Assert.assertEquals(result, "Hello World!!");
+    }
+}
