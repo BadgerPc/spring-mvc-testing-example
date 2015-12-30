6801832341d6e8a3239202963e86e2459884d5c6
Jon Austen <austenjt@gmail.com>
2015-12-29 19:26:08 -0800

    Got unit and integration tests running separate from each other.

---
+++ src/integration-test/java/example/spring/common/controller/UT_ExampleTest.java
@@ -0,0 +1,13 @@
+package example.spring.common.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UT_ExampleTest
+{
+    @Test
+    public void testIt()
+    {
+        Assert.assertTrue(true);
+    }
+}
